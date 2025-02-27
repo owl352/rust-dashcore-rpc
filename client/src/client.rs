a0e409116109111234bb8471a065eec80ebfb6a9
pshenmic <pshenmic@gmail.com>
2025-02-27 14:53:07 +0700

    feat: new getislocks rpc (#58)
    * feat: add getrawislocks method
    
    * fix: rename query method
    
    * feat: add getrawislocks method
    
    * fix: rename query method

--- client/src/client.rs
+++ client/src/client.rs
@@ -446,6 +446,15 @@ pub trait RpcApi: Sized {
         Ok(dashcore::consensus::encode::deserialize(&bytes)?)
     }
 
+    fn get_instant_locks(
+        &self,
+        txids: Vec<&dashcore::Txid>,
+    ) -> Result<Vec<String>> {
+        let mut args = [into_json(txids)?];
+        let instant_locks: Vec<String> = self.call("getislocks", handle_defaults(&mut args, &[null()]))?;
+        Ok(instant_locks)
+    }
+
     fn get_raw_transaction_multi(
         &self,
         transactions_by_block_hash: BTreeMap<&BlockHash, Vec<&dashcore::Txid>>,
